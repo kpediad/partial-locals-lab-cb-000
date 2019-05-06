0d741632ea51c238418d18398df3f9144299a361
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-06 17:58:12 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,6 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(term)
-    Student.all.select{|student| student.name.include?(term)}
+    Student.all.select{|student| student.name.downcase.include?(term.downcase)}
   end
 end
